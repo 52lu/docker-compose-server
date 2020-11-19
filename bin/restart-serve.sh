64c91f50deda923f1e447a2caeae9ce815514cc2
liuqinghui <liuqinghui1991@163.com>
2020-11-19 14:46:41 +0800

    update

--- bin/restart-serve.sh
+++ bin/restart-serve.sh
@@ -1,4 +1,4 @@
-docker-compose stop nginx php-fpm workspace redis  &&
-docker-compose rm -f nginx php-fpm workspace redis &&
-docker-compose build  nginx  php-fpm workspace redis &&
-docker-compose up -d  nginx redis
+docker-compose stop nginx php-fpm workspace   &&
+docker-compose rm -f nginx php-fpm workspace  &&
+docker-compose build  nginx  php-fpm workspace  &&
+docker-compose up -d  nginx
